a9a371bfa489f50fae23e68aff09aba3c1825d86
Your Name <you@example.com>
2023-06-22 13:00:06 +0200

    cors cahnges

--- kurs/aufgaben190623/register.php
+++ kurs/aufgaben190623/register.php
@@ -1,4 +1,15 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+
+echo json_encode("OK");
+
+die();
+
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
